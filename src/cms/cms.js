e0a4c0a2ce42afd76839d07c86b9df5b2a32d3f6
Vincent Weltje <vincent@thriveweb.com.au>
2018-10-23 09:29:37 +1000

    added template files to cms preview

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -1,20 +1,35 @@
-import React from 'react'
-import CMS from 'netlify-cms'
-import './cms-utils'
+import React from "react";
+import CMS from "netlify-cms";
+import "./cms-utils";
 
-import { HomePageTemplate } from '../templates/HomePage'
+import { HomePageTemplate } from "../templates/HomePage";
+import { AboutPageTemplate } from "../templates/AboutPage";
+import { ContactPageTemplate } from "../templates/ContactPage";
+import { ServicesTemplate } from "../templates/ServicesIndex";
 
 if (
-  window.location.hostname === 'localhost' &&
-  window.localStorage.getItem('netlifySiteURL')
+  window.location.hostname === "localhost" &&
+  window.localStorage.getItem("netlifySiteURL")
 ) {
   CMS.registerPreviewStyle(
-    window.localStorage.getItem('netlifySiteURL') + '/styles.css'
-  )
+    window.localStorage.getItem("netlifySiteURL") + "/styles.css"
+  );
 } else {
-  CMS.registerPreviewStyle('/styles.css')
+  CMS.registerPreviewStyle("/styles.css");
 }
 
-CMS.registerPreviewTemplate('home-page', ({ entry }) => (
+CMS.registerPreviewTemplate("home-page", ({ entry }) => (
   <HomePageTemplate {...entry.toJS().data} />
-))
+));
+
+CMS.registerPreviewTemplate("about-page", ({ entry }) => (
+  <AboutPageTemplate {...entry.toJS().data} />
+));
+
+CMS.registerPreviewTemplate("contact-page", ({ entry }) => (
+  <ContactPageTemplate {...entry.toJS().data} />
+));
+
+CMS.registerPreviewTemplate("services-index", ({ entry }) => (
+  <ServicesTemplate {...entry.toJS().data} />
+));

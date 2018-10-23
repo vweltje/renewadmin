2e23cbf3a969522967eb2d504ed0f0517d9daebd
Vincent Weltje <vincent@thriveweb.com.au>
2018-10-23 10:10:57 +1000

    created singleService cms page

---
+++ src/templates/SingleService.js
@@ -0,0 +1,71 @@
+import React from "react";
+import { graphql } from "gatsby";
+
+import Layout from "../components/Layout";
+import "./SingleService.css";
+
+// Export Template for use in CMS preview
+export const SingleServiceTemplate = ({ title }) => {
+  const contentData = {
+    shortDescription: shortDescription,
+    description: description,
+    image: image
+  };
+  return (
+    <main>
+      <section className="section About--TitleSection">
+        <div className="container">
+          <h1>{title}</h1>
+        </div>
+      </section>
+    </main>
+  );
+};
+
+// Export Default SingleService for front-end
+const SingleService = ({ data: { service } }) => {
+  return (
+    <Layout>
+      <SingleServiceTemplate {...service} {...service.frontmatter} />
+    </Layout>
+  );
+};
+
+export default SingleService;
+
+export const pageQuery = graphql`
+  ## Query for SingleService data
+  ## Use GraphiQL interface (http://localhost:8000/___graphql)
+  ## $id is processed via gatsby-node.js
+  ## query name must be unique to this file
+  query SingleService($id: String!) {
+    service: markdownRemark(id: { eq: $id }) {
+      html
+      frontmatter {
+        title
+        shortDescription
+        description
+        image
+        # infoSection {
+        #   title
+        #   infoblocks {
+        #     title
+        #     description
+        #   }
+        # }
+        # getInTouchSection {
+        #   title
+        #   subtitle
+        #   button1 {
+        #     link
+        #     text
+        #   }
+        #   button2 {
+        #     link
+        #     text
+        #   }
+        # }
+      }
+    }
+  }
+`;

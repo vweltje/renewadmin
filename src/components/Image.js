53d6963b898f39160b03befd82b5360524cac1c0
Vincent Weltje <vincent@thriveweb.com.au>
2018-10-24 11:00:41 +1000

    images

--- src/components/Image.js
+++ src/components/Image.js
@@ -1,74 +1,115 @@
-import React from 'react'
+import React from "react";
+import PropTypes from "prop-types";
 
-import './Image.css'
+// import 'intersection-observer'
+// import Observer from '@researchgate/react-intersection-observer'
+
+import "./Image.css";
 
 class Image extends React.Component {
+  // state = {
+  //   isIntersecting: false
+  // }
+  //
+  // handleIntersection = e => {
+  //   console.log(e.isIntersecting)
+  //   if (e.isIntersecting) {
+  //     this.setState({ isIntersecting: true })
+  //   }
+  // }
+
   checkIfIsLocalSrc(src) {
-    if (src.includes('ucarecdn.com')) return false
-    return true
+    if (src.includes("ucarecdn.com")) return false;
+    return true;
   }
 
   render() {
     let {
       background,
-      backgroundSize = 'cover',
-      resolutions = '1000x',
-      className = '',
+      backgroundSize = "cover",
+      resolutions = "1000x",
+      className = "",
       src,
-      srcset = '',
+      // secSet = "",
+      fullSrc,
+      // smallSrc,
       onClick,
-      alt = ''
-    } = this.props
-    const isLocalImg = this.checkIfIsLocalSrc(src)
+      alt = ""
+    } = this.props;
+
+    const isLocalImg = this.checkIfIsLocalSrc(src);
     /* create source set for images */
-    if (!isLocalImg) {
-      srcset = `
-      ${src}-/resize/320x/320.jpg 320w,
-      ${src}-/resize/450x/450.jpg 450w,
-      ${src}-/resize/640x/640.jpg 640w,
-      ${src}-/resize/750x/750.jpg 750w,
-      ${src}-/resize/800x/800.jpg 800w,
-      ${src}-/resize/900x/900.jpg 900w,
-      ${src}-/resize/1000x/-/quality/lighter/1000.jpg 1000w,
-      ${src}-/resize/1200x/-/quality/lighter/1200.jpg 1200w,
-      ${src}-/resize/1500x/-/quality/lighter/1500.jpg 1500w,
-      ${src}-/resize/1600x/-/quality/lighter/1600.jpg 16000w,
-      ${src}-/resize/2000x/-/quality/lightest/2000.jpg 2000w`
-    }
+    // if (!isLocalImg) {
+    //   secSet = `
+    //   ${src}-/progressive/yes/-/format/auto/-/scale_crop/320x/320.jpg 320w,
+    //   ${src}-/progressive/yes/-/format/auto/-/scale_crop/450x/450.jpg 450w,
+    //   ${src}-/progressive/yes/-/format/auto/-/scale_crop/640x/640.jpg 640w,
+    //   ${src}-/progressive/yes/-/format/auto/-/scale_crop/750x/750.jpg 750w,
+    //   ${src}-/progressive/yes/-/format/auto/-/scale_crop/800x/800.jpg 800w,
+    //   ${src}-/progressive/yes/-/format/auto/-/scale_crop/900x/900.jpg 900w,
+    //   ${src}-/progressive/yes/-/format/auto/-/scale_crop/1000x/-/quality/lightest/1000.jpg 1000w,
+    //   ${src}-/progressive/yes/-/format/auto/-/scale_crop/1200x/-/quality/lightest/1200.jpg 1200w,
+    //   ${src}-/progressive/yes/-/format/auto/-/scale_crop/1500x/-/quality/lightest/1500.jpg 1500w,
+    //   ${src}-/progressive/yes/-/format/auto/-/scale_crop/1600x/-/quality/lightest/1600.jpg 16000w,
+    //   ${src}-/progressive/yes/-/format/auto/-/scale_crop/2000x/-/quality/lightest/2000.jpg 2000w`;
+    // }
+
     /* add resolutions options for inline images */
-    if (resolutions === 'small') {
-      resolutions = '800x'
-    } else if (resolutions === 'medium') {
-      resolutions = '1000x'
-    } else if (resolutions === 'large') {
-      resolutions = '2000x'
+    if (resolutions === "small") {
+      resolutions = "800x";
+    } else if (resolutions === "medium") {
+      resolutions = "1000x";
+    } else if (resolutions === "large") {
+      resolutions = "2000x";
     }
 
+    fullSrc = `${src}${
+      isLocalImg
+        ? ""
+        : "-/progressive/yes/-/format/auto/-/resize/" + resolutions + "/"
+    }`;
+    // smallSrc = `${src}${
+    //   isLocalImg ? '' : '-/progressive/yes/-/format/auto/-/resize/10x/'
+    // }`
+
     if (background) {
-      let style = {}
+      let style = {};
       style = {
-        backgroundImage: `url(${src}${isLocalImg ? '' : '/-/resize/2000x/'})`,
+        // backgroundImage: `url(${
+        //   this.state.isIntersecting ? fullSrc : smallSrc
+        // })`,
+        backgroundImage: `url(${fullSrc})`,
         backgroundSize
-      }
+      };
       return (
+        // <Observer onChange={this.handleIntersection}>
         <div
           className={`BackgroundImage absolute ${className}`}
           style={style}
         />
-      )
+        // </Observer>
+      );
     }
 
     return (
+      // <Observer onChange={this.handleIntersection}>
       <img
-        className={`Image ${className}`}
-        src={`${src}${isLocalImg ? '' : '/-/resize/' + resolutions + '/'}`}
-        srcSet={srcset}
-        sizes={'100vw'}
+        className={`Image LazyImage ${className}`}
+        src={fullSrc}
+        // srcSet={secSet}
+        // src={this.state.isIntersecting ? fullSrc : smallSrc}
+        // srcSet={this.state.isIntersecting ? secSet : ''}
+        sizes={"100vw"}
         onClick={onClick}
         alt={alt}
       />
-    )
+      // </Observer>
+    );
   }
 }
 
-export default Image
+Image.propTypes = {
+  alt: PropTypes.string.isRequired
+};
+
+export default Image;

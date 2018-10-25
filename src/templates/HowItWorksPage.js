09d7360a15f2b9dbc3b1857e602a0f92d458da54
Vincent Weltje <vincent@thriveweb.com.au>
2018-10-25 17:08:33 +1000

    added placeholder to missing content on how it works page (just for me to remember that i still need to implement that bit)

--- src/templates/HowItWorksPage.js
+++ src/templates/HowItWorksPage.js
@@ -1,11 +1,11 @@
-import React from "react";
-import { graphql } from "gatsby";
+import React from 'react'
+import { graphql } from 'gatsby'
 
-import Layout from "../components/Layout";
-import ContentBlock from "../components/ContentBlock";
-import ServicesGrid from "../components/ServicesGrid";
+import Layout from '../components/Layout'
+import ContentBlock from '../components/ContentBlock'
+import ServicesGrid from '../components/ServicesGrid'
 
-import "./HowItWorksPage.css";
+import './HowItWorksPage.css'
 
 // Export Template for use in CMS preview
 export const HowItWorksPageTemplate = ({
@@ -19,30 +19,40 @@ export const HowItWorksPageTemplate = ({
     shortDescription: shortDescription,
     description: description,
     image: image
-  };
+  }
+  let styles = {
+    width: '100%',
+    height: '650px',
+    background: '#33495b'
+  }
   return (
     <main>
       <section className="section About--TitleSection">
         <div className="container">
           <h1>{title}</h1>
           <ContentBlock content={contentData} />
+          <section className="section Home--CaseStudiesSection">
+            <div className="container">
+              <div style={styles}>placeholder</div>
+            </div>
+          </section>
           <ServicesGrid services={services} />
         </div>
       </section>
     </main>
-  );
-};
+  )
+}
 
 // Export Default HowItWorksPage for front-end
 const HowItWorksPage = ({ data }) => {
-  const page = data.page;
-  let services = [];
+  const page = data.page
+  let services = []
   data.services.edges.map((service, index) => {
     return services.push({
       ...service.node.fields,
       ...service.node.frontmatter
-    });
-  });
+    })
+  })
   return (
     <Layout>
       <HowItWorksPageTemplate
@@ -51,10 +61,10 @@ const HowItWorksPage = ({ data }) => {
         services={services}
       />
     </Layout>
-  );
-};
+  )
+}
 
-export default HowItWorksPage;
+export default HowItWorksPage
 
 export const pageQuery = graphql`
   ## Query for HowItWorksPage data
@@ -87,4 +97,4 @@ export const pageQuery = graphql`
       }
     }
   }
-`;
+`

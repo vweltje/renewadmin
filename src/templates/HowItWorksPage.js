994c22866d5867697698e002998e6c48b4415a10
Vincent Weltje <vincentweltje@Vincents-iMac.local>
2018-11-14 14:46:19 +1000

    updated how it works graphics

--- src/templates/HowItWorksPage.js
+++ src/templates/HowItWorksPage.js
@@ -3,98 +3,11 @@ import Helmet from 'react-helmet'
 import { graphql } from 'gatsby'
 
 import Layout from '../components/Layout'
+import Image from '../components/Image'
 import ContentBlock from '../components/ContentBlock'
 import ServicesGrid from '../components/ServicesGrid'
 
 import './HowItWorksPage.css'
-
-export class StepSection extends React.Component {
-  static defaultProps = {
-    services: {}
-  }
-
-  state = {
-    activeService: 0
-  }
-
-  handleServiceNavClick = index => {
-    const activeService =
-      this.state.activeService === index ? this.state.activeService : index
-    this.setState({ activeService })
-  }
-
-  getIconSrc(slug) {
-    let icon
-    if (slug.includes('account-payable')) icon = 'payable'
-    else if (slug.includes('accounts-receivable')) icon = 'receive'
-    else if (slug.includes('bas')) icon = 'bas'
-    else if (slug.includes('month-end')) icon = 'month'
-    else if (slug.includes('payg')) icon = 'pay'
-    else if (slug.includes('payroll')) icon = 'payroll'
-    else if (slug.includes('reconciliations')) icon = 'reconciliantion'
-    else if (slug.includes('superannuation')) icon = 'super'
-    else if (slug.includes('work-flow-efficiency')) icon = 'flow'
-    return icon
-  }
-
-  render() {
-    let services =
-      !!this.props.services &&
-      this.props.services.length &&
-      this.props.services.filter(item => item.status === 'Featured')
-
-    return (
-      <div className="ServiceSteps">
-        <div className="ServiceStepsNav">
-          {!!services &&
-            services.map((service, i) => {
-              return (
-                <div
-                  className={
-                    'ServiceNavItem ' +
-                    (this.state.activeService === i ? 'active' : '')
-                  }
-                  key={'ServiceNavItem-' + i}
-                  onClick={() => this.handleServiceNavClick(i)}
-                >
-                  <div
-                    className={'Service--Icon ' + this.getIconSrc(service.slug)}
-                  />
-                  <span>{service.title}</span>
-                </div>
-              )
-            })}
-        </div>
-        <div className="ServiceStepsContentBlocks">
-          {!!services &&
-            services.map((service, i) => {
-              return (
-                <div
-                  className={
-                    'ServiceStepsContentBlock ' +
-                    (this.state.activeService === i ? 'active' : '')
-                  }
-                  key={'ServiceStepsContentBlock-' + i}
-                >
-                  {service.howItWorks.steps.map((step, num) => {
-                    return (
-                      <div
-                        className="ServiceStepsContentBlockItem"
-                        key={'step-' + num}
-                      >
-                        <span>{num + 1}</span>
-                        <p>{step.title}</p>
-                      </div>
-                    )
-                  })}
-                </div>
-              )
-            })}
-        </div>
-      </div>
-    )
-  }
-}
 
 // Export Template for use in CMS preview
 export const HowItWorksPageTemplate = ({
@@ -125,7 +38,18 @@ export const HowItWorksPageTemplate = ({
               <Fragment>
                 <section className="section Home--CaseStudiesSection">
                   <div className="container">
-                    <StepSection services={services} />
+                    <div className="Howitworks--GraphSection">
+                      <Image
+                        className="default"
+                        src="/images/howitworks_graphic.svg"
+                        alt="how it works graphic"
+                      />
+                      <Image
+                        className="mobile"
+                        src="/images/howitworks_graphic_mobile.svg"
+                        alt="how it works graphic"
+                      />
+                    </div>
                   </div>
                 </section>
                 <ServicesGrid services={services} />

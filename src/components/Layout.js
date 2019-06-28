e70b64734812de9f275c55a9c6d46f24033f4015
Alex <alex@thriveweb.com.au>
2019-06-28 10:50:59 +1000

    ga code

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -39,6 +39,30 @@ export default ({ children, meta }) => {
                 href="https://fonts.googleapis.com/css?family=Montserrat:200,400,500,700|Open+Sans:400,400i,600,700"
                 rel="stylesheet"
               />
+              <script
+                async
+                src="https://www.googletagmanager.com/gtag/js?id=UA-123975389-29"
+              />
+
+              <script>
+                {`
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+
+                gtag('config', 'UA-123975389-29');
+                `}
+              </script>
+
+              <meta
+                name="msvalidate.01"
+                content="B0112812CA622A13C327AF572E2BA5B5"
+              />
+
+              <meta
+                name="google-site-verification"
+                content="VBJq1UsJ2Rw5F17d62S9Vjcn2J7VLRY57bHCFRu_h5o"
+              />
             </Helmet>
 
             <Meta
